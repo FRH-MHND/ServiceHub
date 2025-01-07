2349c91eb112f5cefbccd4f2a7e7b18e862ae2d2
Farah Abd Aljawad <f.aljawad@Nafithhq.com>
2025-01-07 17:22:05 +0300

    final

---
+++ ServiceHub/Migrations/20250107091140_AddPasswordHashToUsers.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServiceHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordHashToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "ServiceProviders",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "ServiceProviders",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ServiceCategory",
+                table: "ServiceProviders",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "ServiceProviders");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "ServiceProviders");
+
+            migrationBuilder.DropColumn(
+                name: "ServiceCategory",
+                table: "ServiceProviders");
+        }
+    }
+}
--- ServiceHub/obj/Debug/net8.0/ServiceHub.AssemblyInfo.cs
+++ ServiceHub/obj/Debug/net8.0/ServiceHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea3f7dd7384a0c332c8026de5b23a66641e5de57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44333d3f30bcef4e3d251ada8a416b052995e9dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
